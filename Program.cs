39c7effb86eb8efb9a562eda0a49a26245a61126
uzkid <ibowik694@gmail.com>
2024-08-07 13:39:23 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the radius: ");
+        if (double.TryParse(Console.ReadLine(), out double radius))
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            double circumference = 2 * Math.PI * radius;
+
+            Console.WriteLine($"S={area}, L={circumference}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number for the radius.");
+        }
+    }
+}
